8ecfb16d841469ef42525a0e570b5fe5352d0ad2
shadowghost94 <salifououzerou25@gmail.com>
2022-12-30 16:25:28 +0100

    try to upload online

---
+++ first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int variable;
+	const int max=100, min=1;
+	srand(time(NULL));
+	printf("%d \n\n", variable);
+	return 0;
+
+}
