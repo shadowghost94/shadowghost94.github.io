2a763b7601ae2630ca5296af8bdcaf31a14da391
shadowghost94 <salifououzerou25@gmail.com>
2023-01-18 23:40:32 +0100

    18_01_23

--- firstJS.js
+++ firstJS.js
@@ -1,4 +1,6 @@
 var names= prompt("Veuillez entrer un le mot d'identification ! ");
 if (names="Hussein"){
-    alert("Bienvenue bg, trouve de nouvelle fonctionnalite !");
+    document.write("Bienvenue bg, hesite pas a me faire des propositions de pour ameliorer mon site");
+}else{
+    document.write("vous n'avez le mot d'autorisation mais visitez quand meme, si vous ete ici, c'est forcement pour une raison");
 }
